f03fe1488b4f97139b7365311f4f427c83527438
Oskar Niburski <oskarniburski@gmail.com>
2016-05-23 22:09:26 -0400

    Image rewrite

--- app/components/Img/index.js
+++ app/components/Img/index.js
@@ -8,17 +8,29 @@
 
 import React, { PropTypes } from 'react';
 
-function Img(props) {
-  return (
-    <img className={props.className} src={props.src} alt={props.alt} />
-  );
+export default class Img extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  onImageLoad() {
+    if (this.isMounted()) {
+      this.setState({ loaded: true });
+    }
+  }
+
+  render() {
+    return (
+      <img className={this.props.className} src={this.props.src} />
+    )
+  }
 }
 
 // We require the use of src and alt, only enforced by react in dev mode
 Img.propTypes = {
   src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
-  className: PropTypes.string,
+  className: PropTypes.string
 };
 
 export default Img;
--- app/components/Page/index.js
+++ app/components/Page/index.js
@@ -1,3 +1,12 @@
+// <Image className={styles.logo} src={Tree5} />
+// <Image className={styles.logo} src={Tree4} />
+// <Image className={styles.logo} src={Tree3} />
+// <Image className={styles.logo} src={Tree2} />
+// <Image className={styles.logo} src={Tree1} />
+// <Image className={styles.logo} src={Sun} />
+// <Image className={styles.logo} src={Clouds} />
+// <Image className={styles.logo} src={Birds} />
+
 import React from 'react';
 import { Button, Jumbotron, Row, Col, Image } from 'react-bootstrap';
 import Background from './bikingbackground_RS.jpg';
@@ -36,14 +45,10 @@ export default class Page extends React.Component {
               </Col>
             </Row>            
             <Image className={styles.logo} src={Background} />
-            <Image className={styles.logo} src={Tree5} />
-            <Image className={styles.logo} src={Tree4} />
-            <Image className={styles.logo} src={Tree3} />
-            <Image className={styles.logo} src={Tree2} />
-            <Image className={styles.logo} src={Tree1} />
-            <Image className={styles.logo} src={Sun} />
-            <Image className={styles.logo} src={Clouds} />
-            <Image className={styles.logo} src={Birds} />
+
+            <Img className={styles.logo} src={Tree2} />
+            <Img className={styles.logo} src={Tree1} />
+            
           </Col>        
         </Row>
         <Row className={styles.site_row}>
