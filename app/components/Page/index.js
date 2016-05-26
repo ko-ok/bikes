c4f11f18d1d4fb8e65ffacb08bfe485a42c1dd88
Oskar Niburski <oskarniburski@gmail.com>
2016-05-26 09:16:01 -0400

    Vid styles

--- app/components/Page/index.js
+++ app/components/Page/index.js
@@ -33,22 +33,24 @@ export default class Page extends React.Component {
     });
 
     feed.run();
+
+    document.getElementById('video').play()
   }
 
   render() {
     return (
       <div>
         <Row>
-          <Col xs={12} md={12}>            
+          <Col md={12}>            
             <Row>
-              <Col xs={12} md={12}>
+              <Col md={12}>
                 <div className={styles.header}>
                   <h1 className={styles.header_text}>Ease to the East</h1>           
                 </div>
               </Col>
             </Row>
             <Row>
-              <Col xs={12} md={12}>
+              <Col md={12}>
                 <div className={styles.nav}>
                   <a href="#who" className={styles.a}>Who</a>
                   <a href="#what" className={styles.a}>What</a>
@@ -68,13 +70,13 @@ export default class Page extends React.Component {
           </Col>        
         </Row>
         <Row id="who" className="section">
-          <Col xs={12} md={12}>
+          <Col md={12}>
             <Row>
-              <Col className="hi" xs={6} md={6}>
+              <Col className="hi" md={6}>
                 <h1>Hi.</h1>
                 <h3>We're twins traveling to Halifax on bike. We want you to come along with us - huffing, sweaty, and full of undercooked beans. We'll save you a bite too.</h3>
               </Col>
-              <Col xs={6} md={6}>
+              <Col md={6}>
                 <Image className="img-row waving" src={require('./../../assets/croppedwaving.gif')} />
                 <Image className="img-row glasses" src={require('./../../assets/face2.png')} />
               </Col>
@@ -82,13 +84,13 @@ export default class Page extends React.Component {
           </Col>        
         </Row>
         <Row id="what" className="section">
-          <Col xs={12} md={12}>
+          <Col md={12}>
             <Row>
-              <Col xs={6} md={6}>
+              <Col md={6}>
                 <h1 id="way">THE WAY</h1>
                 <Image className="img-row" src={require('./../../assets/trail2BOTHRIDERS.gif')} />
               </Col>
-              <Col xs={6} md={6}>                
+              <Col md={6}>                
                 <h1 id="wow">THE WOW</h1>
                 <div id="instafeed"></div>
 
@@ -99,32 +101,32 @@ export default class Page extends React.Component {
           </Col>        
         </Row>
         <Row id="where" className="section">
-          <Col xs={12} md={12}>
+          <Col md={12}>
             <Row>
-              <Col xs={6} md={6}>
+              <Col md={6}>
                 <h1 className="reason">THE REASON</h1>            
                 <p className="reason-p"> We were born silent as clamshells. Three months premature, out of water too soon. We got lucky though, 90 odd days in intensive care, lovely parents, a lifetime to learn how to open and be open ourselves</p>
                 <p className="reason-p"> Yet as we find some different yet still quiet clamsells on this trip, we hope to help in small handfuls. We will be raising money for the Canadian Premature Babies Foundation. Please feel free to donate. Nothing is too little, the big evidence being found in us.</p>
                 <button className="donate"> Donate </button>
               </Col>
-              <Col xs={6} md={6}>
+              <Col md={6}>
                 <Row>
-                  <Col xs={12} md={12}>
+                  <Col md={12}>
                     <Image className="img-row" src={require('./../../assets/hosp.png')} />
                   </Col>
                 </Row>
                 <Row>
-                  <Col xs={6} md={6}>
-                    <Image className="img-row" src={require('./../../assets/carbabies.jpg')} />
+                  <Col className="pic-col" md={6}>
+                    <Image className="car-babies img-row" src={require('./../../assets/carbabies.jpg')} />
                   </Col>
-                  <Col xs={6} md={6}>
+                  <Col className="pic-col" md={6}>
                     <Row> 
-                      <Col xs={12} md={12}>
+                      <Col md={12}>
                         <Image className="img-row" src={require('./../../assets/legraised.jpg')} />
                       </Col>
                     </Row>
                     <Row> 
-                      <Col xs={12} md={12}>
+                      <Col md={12}>
                         <Image className="img-row" src={require('./../../assets/reckless.jpg')} />
                       </Col>
                     </Row>
@@ -135,14 +137,9 @@ export default class Page extends React.Component {
           </Col>        
         </Row>
         <Row id="footer" className="section">
-          <Col xs={12} md={12}>
+          <Col md={12}>
             <div className="video_container">
-              <div className="video_overlay">
-                <h1> Subscribe for weekly updates!</h1>
-                <input type="email"/>
-                <button> Submit </button>
-              </div>           
-              <video autoplay controls muted loop>
+              <video id="video" autoplay muted loop>
                 <source src={require('./../../assets/Biking1.mp4')} type="video/mp4" />
               </video>   
             </div>
