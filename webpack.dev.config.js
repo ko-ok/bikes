208110a8d9462b3246ecfabe767acbef51d0f073
Oskar Niburski <oskarniburski@gmail.com>
2016-06-01 21:56:36 -0400

    Ugh

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,9 +2,10 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'eval',
 
   entry: [
+    'webpack-hot-middleware/client',
     './src/index'
   ],
 
@@ -15,25 +16,16 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
 
   module: {
     loaders: [
       { test: /\.js?$/,
         loader: 'babel',
-        include: path.join(__dirname, 'src') },
+        include: path.join(__dirname, 'src')
+      },
       { test: /\.scss?$/,
         loader: 'style!css!sass',
         include: path.join(__dirname, 'src', 'styles') },
